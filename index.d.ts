2b53cf182dd1abc23764b881a3697af19087264c
Clement G <contact@clemdesign.fr>
2018-04-15 20:54:26 +0200

    Transform package for NPM

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+/**
+ * TsGeo library
+ *
+ * @author    clemdesign <contact@clemdesign.fr>
+ * @license   https://opensource.org/licenses/MIT
+ * @link      https://github.com/clemdesign/typescript-tsgeo
+*/
+
+export * from './lib';
